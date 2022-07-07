d9cea970e2a0d25f69c5a56a9c4d6143dfa72613
d0422 <rlfehd2013@naver.com>
2022-07-07 20:20:09 +0900

    0707

---
+++ typescript/build/index.js
@@ -0,0 +1,14 @@
+import { init, exit } from "myPackage";
+init({
+    url: "true",
+});
+exit(1);
+const hello = () => "hi";
+class Block {
+    constructor(data) {
+        this.data = data;
+    }
+    static hello() {
+        return "hi";
+    }
+}
---
+++ typescript/src/index.ts
@@ -0,0 +1,14 @@
+import {init ,exit} from "myPackage";
+init({
+    url:"true",
+});
+exit(1);
+const hello = ()=>"hi";
+class Block{
+    constructor(
+        private data:string
+    ){}
+    static hello(){
+        return "hi";
+    }
+}
